00f52aa47ff6af4d62d899b4886145f0934bd83e
sl4 <3940@holbertonschool.com>
2022-05-16 15:13:48 -0700

    todos page now loads only once

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,13 +8,12 @@ if __name__ == "__main__":
     user = {}
     tasks = []
 
+    rtodo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     ruser = requests.get('https://jsonplaceholder.typicode.com/users').json()
     for u in ruser:
         ide = u.get("id")
         username = u.get("username")
 
-        rtodo = requests.get(
-            'https://jsonplaceholder.typicode.com/todos').json()
         for elem in rtodo:
             if elem.get("userId") == int(ide):
                 tasks.append({"task": elem.get("title"),
