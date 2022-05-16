d5e50bae1cab4c63f7bb88e9df0a7c2150236431
sl4 <3940@holbertonschool.com>
2022-05-16 14:40:35 -0700

    corrected dictionary insertion in list

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """export json module"""
-import requests
 import json
-from sys import argv
+import requests
+import sys
 
 
 if __name__ == "__main__":
     total = done = 0
-    uid = argv[1]
+    uid = sys.argv[1]
     user = {}
     tasks = []
     task = {}
@@ -21,11 +21,9 @@ if __name__ == "__main__":
     rtodo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     for elem in rtodo:
         if elem.get("userId") == int(uid):
-            task["username"] = username
-            task["completed"] = elem.get("completed")
-            task["task"] = elem.get("title")
-            tasks.append(task)
-
+            tasks.append({"task": elem.get("title"),
+                          "completed": elem.get("completed"),
+                          "username": username})
     user[uid] = tasks
     with open(uid + '.json', 'a') as f:
         json.dump(user, f)
