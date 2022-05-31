f6f12e97a9e07d54128b05cae6eb3e87bc2fc201
sl4 <3940@holbertonschool.com>
2022-05-31 11:10:06 -0700

    checked for valid subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,5 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     """how many subs function"""
     subs = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
-                        headers={'User-Agent': 'me'}).json()
+                        headers={'User-Agent': 'me'})
+    if subs.status_code == 404:
+        return 0
+    else:
+        subs = subs.json()
     return subs['data']['subscribers']
